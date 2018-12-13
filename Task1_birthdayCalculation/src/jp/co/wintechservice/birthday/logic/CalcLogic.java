400c072fa1ff91be9b34a1f1579e9eb0ef449f02
KohariJunichiro <kohari.junichirou@wintechservice.co.jp>
2018-12-13 14:33:47 +0900

    誕生日計算の課題を実施

--- Task1_birthdayCalculation/src/jp/co/wintechservice/birthday/logic/CalcLogic.java
+++ Task1_birthdayCalculation/src/jp/co/wintechservice/birthday/logic/CalcLogic.java
@@ -1,10 +1,40 @@
 package jp.co.wintechservice.birthday.logic;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+
 /**
  * 誕生日計算の計算ロジック
  * @author
  *
  */
 public class CalcLogic {
+
+    public void logic (LocalDate ld) {
+        /**
+         * 時間、分の間隔
+         */
+        LocalDateTime birthday = ld.atTime(00, 00, 00);
+        LocalDateTime today = LocalDateTime.now();
+        Duration hourAndMinute = Duration.between(birthday, today);
+        long hour = hourAndMinute.toHours();
+        long minute = hourAndMinute.toMinutes();
+        
+        /**
+         * 月、日の間隔
+         */
+        LocalDate todayld = today.toLocalDate();
+        Period monthAndDay = Period.between(ld, todayld);
+        int year = monthAndDay.getYears();
+        int month = year * 12 + monthAndDay.getMonths();
+        long day = hour /24;
+
+        System.out.println("今日はあなたが生まれてから" + month + "ヶ月目です。");
+        System.out.println("今日はあなたが生まれてから" + day + "日目です。");
+        System.out.println("現時刻はあなたが生まれてから" + hour + "時間です。");
+        System.out.println("現時刻はあなたが生まれてから" + minute + "分です。");
+    }
 
 }
--- Task1_birthdayCalculation/src/jp/co/wintechservice/birthday/main/CalcMain.java
+++ Task1_birthdayCalculation/src/jp/co/wintechservice/birthday/main/CalcMain.java
@@ -1,6 +1,27 @@
 package jp.co.wintechservice.birthday.main;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+import jp.co.wintechservice.birthday.logic.CalcLogic;
+
+
+
+
 /**
+ *
+ * コンソールから生年月日を入力し、以下の情報を出力するプログラムを作成せよ。
+ * 入力形式はYYYY/MM/DDの形式とし、この形式以外の入力値はエラーとする。
+ * 出力情報(○に計算結果を埋め込み、下記文面通りに出力)
+ * ・今日はあなたが生まれてから○ヶ月目です。
+ * ・今日はあなたが生まれてから○日目です。
+ * ・現時刻はあなたが生まれてから○時間です。
+ * ・現時刻はあなたが生まれてから○分です。
+ * エラー文言
+ * ・日付として正しい値を入力してください。
+ *
  * 誕生日計算実行クラス
  * @author
  *
@@ -11,8 +32,25 @@ public class CalcMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	    /**
+	     *  生年月日を入力してもらう
+	     */
+	   LocalDate ld = null;
+       Scanner birthdayInput = new Scanner(System.in);
+       System.out.println("あなたの生年月日をYYYY/MM/DDの形式で入力して下さい");
+       /**
+        * YYYY/MM/DDの形式以外の入力値はエラー
+        */
+       try {
+           DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+           String birthdayString = birthdayInput.nextLine();
+           ld = LocalDate.parse(birthdayString, dtf);
+           
+       } catch (DateTimeParseException e) {
+           System.out.println("日付として正しい値を入力してください。");
+       }
+       CalcLogic cl = new CalcLogic();
+       cl.logic(ld);
 
 	}
-
 }
